205340fc1eb06a37f2938dc7ec7c93aabc5206b8
Danielly Nunes <danym.nunes@gmail.com>
2022-08-08 18:02:39 -0300

    finalização repository

--- projetointegrador/src/main/java/org/generation/projetointegrador/repository/TemaRepository.java
+++ projetointegrador/src/main/java/org/generation/projetointegrador/repository/TemaRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.generation.projetointegrador.model.TemaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TemaRepository  extends JpaRepository<TemaModel, Long> {
 
 	public List<TemaModel> findAllByDescricaoContainingIgnoreCase (@Param ("descrição") String descricao);
