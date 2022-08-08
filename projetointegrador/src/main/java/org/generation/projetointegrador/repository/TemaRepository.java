1ffc9ce5bc12ac4078b11353eaf85e3bc755da39
Danielly Nunes <danym.nunes@gmail.com>
2022-08-08 17:43:17 -0300

    atualização repository

---
+++ projetointegrador/src/main/java/org/generation/projetointegrador/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.projetointegrador.repository;
+
+import java.util.List;
+
+import org.generation.projetointegrador.model.TemaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface TemaRepository  extends JpaRepository<TemaModel, Long> {
+
+	public List<TemaModel> findAllByDescricaoContainingIgnoreCase (@Param ("descrição") String descricao);
+
+}
