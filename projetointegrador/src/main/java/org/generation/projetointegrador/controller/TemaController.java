d8bde69c65e4df0d394424b81111eb9fc18973e1
Danielly Nunes <danym.nunes@gmail.com>
2022-08-12 14:37:05 -0300

    correção

--- projetointegrador/src/main/java/org/generation/projetointegrador/controller/TemaController.java
+++ projetointegrador/src/main/java/org/generation/projetointegrador/controller/TemaController.java
@@ -4,11 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-<<<<<<< HEAD
 import org.generation.projetointegrador.model.TemaModel;
 import org.generation.projetointegrador.repository.TemaRepository;
-=======
->>>>>>> d5622626cc3a5698bc01fe1ae3983e0725807c90
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,33 +18,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-<<<<<<< HEAD
-@RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping("/tema")
-public class TemaController {
-	
-	@Autowired
-	private TemaRepository repository;
-	
-	@GetMapping 
-	public ResponseEntity<List<TemaModel>> getAll (){
-		return ResponseEntity.ok(repository.findAll());
-		
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<TemaModel> getById (@PathVariable Long id) {
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.notFound().build());
-	}
-	
-	@GetMapping ("/descricao/{descricao}")
-	public ResponseEntity<List<TemaModel>> getByDescricao (@PathVariable String descricao) {
-=======
-import org.generation.projetointegrador.model.TemaModel;
-import org.generation.projetointegrador.repository.TemaRepository;
 
 @RestController
 @CrossOrigin (origins = "*", allowedHeaders = "*")
@@ -68,12 +38,10 @@ public class TemaController {
 	
 	@GetMapping ("/descricao/{descricao}")
 	public ResponseEntity<List<TemaModel>> getByDescricao (@PathVariable String descricao){
->>>>>>> d5622626cc3a5698bc01fe1ae3983e0725807c90
 		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
 	
 	@PostMapping
-<<<<<<< HEAD
 	public ResponseEntity<TemaModel> post (@RequestBody @Valid TemaModel tema) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 		
@@ -89,23 +57,5 @@ public class TemaController {
 	public void delete (@PathVariable Long id) {
 		repository.deleteById(id);
 	}
-	
 
-  
-=======
-		public ResponseEntity<TemaModel> post (@RequestBody @Valid TemaModel tema){
-			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
-	}
-	
-	@PutMapping
-	public ResponseEntity<TemaModel> put (@RequestBody TemaModel tema){
-		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
-	}
-	
-	@DeleteMapping ("/{id}")
-		public void delete (@PathVariable Long id) {
-		repository.deleteById(id);
-	}
-	
->>>>>>> d5622626cc3a5698bc01fe1ae3983e0725807c90
 }
--- projetointegrador/src/main/java/org/generation/projetointegrador/repository/TemaRepository.java
+++ projetointegrador/src/main/java/org/generation/projetointegrador/repository/TemaRepository.java
@@ -8,14 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
-public interface TemaRepository extends JpaRepository<TemaModel, Long> {
-
-	public List<TemaModel> findAllByDescricaoContainingIgnoreCase(@Param ("descrição") String descricao);
-=======
 public interface TemaRepository  extends JpaRepository<TemaModel, Long> {
 
 	public List<TemaModel> findAllByDescricaoContainingIgnoreCase (@Param ("descrição") String descricao);
-
->>>>>>> d5622626cc3a5698bc01fe1ae3983e0725807c90
 }
