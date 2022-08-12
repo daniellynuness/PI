d5622626cc3a5698bc01fe1ae3983e0725807c90
thayna <3therer@gmail.com>
2022-08-12 11:53:57 -0300

    atualização projeto

--- projetointegrador/src/main/java/org/generation/projetointegrador/model/UsuarioModel.java
+++ projetointegrador/src/main/java/org/generation/projetointegrador/model/UsuarioModel.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -23,16 +24,18 @@ public class UsuarioModel {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull(message = "O atributo email é obrigatório")
-	private String email;
-	
-	@NotBlank(message = "O atributo senha é obrigatório e não pode utilizar espaços em brancos!")
-	@Size(min = 6, max = 10 , message = "O atributo senha deve conter no mínimo 05 e no máximo 10 caracteres")
-	private String senha;
-	
-	@NotNull
+	@NotNull (message = "O Atributo Nome é Obrigatório!")
 	private String nome;
 	
+	@NotNull(message = "O Atributo Usuário é obrigatório!")
+	@Email(message= "O Atributo Usuário deve ser um email válido!")
+	private String usuario;
+	
+	@NotBlank(message = "O atributo Senha é Obrigatório!")
+	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
+	private String senha;
+	
+	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
 	
 	@OneToMany (mappedBy= "usuario", cascade = CascadeType.ALL)
@@ -47,12 +50,20 @@ public class UsuarioModel {
 		this.id = id;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
 	}
 
 	public String getSenha() {
@@ -61,14 +72,6 @@ public class UsuarioModel {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
 	}
 
 	public String getFoto() {
@@ -86,10 +89,6 @@ public class UsuarioModel {
 	public void setPostagem(List<PostagemModel> postagem) {
 		this.postagem = postagem;
 	}
-	
-	
-	
-	
-	
+
 	
 }
