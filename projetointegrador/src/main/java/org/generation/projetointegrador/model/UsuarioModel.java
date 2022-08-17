3399af8220b6ff949b0da142f30b2eeb2fd06c5e
Beatrizrochelly10@gmail.com <beatrizrochelly10@gmail.com>
2022-08-17 14:55:05 -0300

    Deploy do Projeto Integrador

--- projetointegrador/src/main/java/org/generation/projetointegrador/model/UsuarioModel.java
+++ projetointegrador/src/main/java/org/generation/projetointegrador/model/UsuarioModel.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name= "tb_usuario")
 public class UsuarioModel {
@@ -27,6 +29,8 @@ public class UsuarioModel {
 	@NotNull (message = "O Atributo Nome é Obrigatório!")
 	private String nome;
 	
+	
+	@Schema(example = "email@email.com.br") 
 	@NotNull(message = "O Atributo Usuário é obrigatório!")
 	@Email(message= "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
