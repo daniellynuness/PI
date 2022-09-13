09e78f56e76bf50c80004291c1f401f3d2b63335
Danielly Nunes <danym.nunes@gmail.com>
2022-09-13 04:20:22 -0300

    atualizações necessarias no usuario service

--- src/main/java/org/generation/projetointegrador/service/UsuarioService.java
+++ src/main/java/org/generation/projetointegrador/service/UsuarioService.java
@@ -62,7 +62,8 @@ public class UsuarioService {
 	                usuarioLogin.get().setNome(usuario.get().getNome());
 	                usuarioLogin.get().setFoto(usuario.get().getFoto());
 	                usuarioLogin.get().setTipo(usuario.get().getTipo());
-	                usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(),        usuarioLogin.get().getSenha()));
+	                usuarioLogin.get().setDocumento(usuario.get().getDocumento());
+	                usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(),usuarioLogin.get().getSenha()));
 	                usuarioLogin.get().setSenha(usuario.get().getSenha());
 
 	                return usuarioLogin;
